21e1419c365dd8fe041cf99e00244bdecb566a11
ilvetrov <ilya.ilvetrov@gmail.com>
2021-11-21 02:08:25 +0400

    turn on quiz

--- wp-content/themes/u-ilve/template-parts/quiz/index.php
+++ wp-content/themes/u-ilve/template-parts/quiz/index.php
@@ -1,7 +1,6 @@
 <div class="quiz">
   <div class="quiz__blocks" data-quiz="<?php echo $args['name']; ?>">
     <?php $quiz_questions = carbon_get_cached_theme_option('quiz_questions'); ?>
-    <?php $quiz_questions = []; ?>
     <?php foreach ($quiz_questions as $question_order => $question): ?>
       <?php $question_name = $args['name'] . '-' . $question_order; ?>
       <div class="quiz__block <?php echo ($question_order > 0 ? 'hidden-right obedient' : ''); ?>" data-quiz-block="<?php echo $question_order; ?>" data-quiz-of="<?php echo $args['name']; ?>">
@@ -30,8 +29,7 @@
       </div>
       <!-- /.quiz__block -->
     <?php endforeach; ?>
-    <!-- <div class="quiz__block hidden-right obedient" data-quiz-block="<?php echo count($quiz_questions); ?>" data-quiz-of="<?php echo $args['name']; ?>"> -->
-    <div class="quiz__block" data-quiz-block="<?php echo count($quiz_questions); ?>" data-quiz-of="<?php echo $args['name']; ?>">
+    <div class="quiz__block hidden-right obedient" data-quiz-block="<?php echo count($quiz_questions); ?>" data-quiz-of="<?php echo $args['name']; ?>">
       <?php get_template_part('template-parts/quiz/parts/final', '', $args); ?>
     </div>
     <!-- /.quiz__block -->
