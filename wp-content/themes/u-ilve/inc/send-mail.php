0f103979bf331990d5b349edb54d46906f9da52e
ilvetrov <ilya.ilvetrov@gmail.com>
2021-12-10 18:03:49 +0400

    email view fix

--- wp-content/themes/u-ilve/inc/send-mail.php
+++ wp-content/themes/u-ilve/inc/send-mail.php
@@ -4,11 +4,9 @@ function sendMail($phone, $additional = [])
 {
   $recipients = carbon_get_cached_theme_option('lead_recipients');
   if ($recipients) {
-    $emailSubject = @$additional['subject'] ? $additional['subject'] : 'Новая заявка с сайта';
+    $emailSubject = @$additional['subject'] ? $additional['subject'] : 'Новая заявка с сайта ' . $_SERVER['SERVER_NAME'];
     
-    return wp_mail($recipients, $emailSubject, getMailHtml($phone, $additional), [
-      'content-type' => 'text/html'
-    ]);
+    return wp_mail($recipients, $emailSubject, getMailHtml($phone, $additional), ['Content-Type: text/html; charset=UTF-8']);
   }
 }
 function getMailHtml($phone, Array $additional = [])
