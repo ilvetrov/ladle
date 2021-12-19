593f829d2115e2cef6293e089d16b1b3a3189d0e
ilvetrov <ilya.ilvetrov@gmail.com>
2021-12-19 21:44:26 +0400

    change default g tag

--- wp-content/themes/u-ilve/inc/carbon-fields/theme-options.php
+++ wp-content/themes/u-ilve/inc/carbon-fields/theme-options.php
@@ -43,16 +43,16 @@ function crb_attach_theme_options()
       ->set_rows(10)
       ->set_help_text('Вставьте код всех метрик для сайта')
       ->set_default_value('
-      <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(86836258, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/86836258" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+        <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(86836258, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/86836258" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 
-        <!-- Global site tag (gtag.js) - Google Ads: 10823913442 -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10823913442"></script>
+        <!-- Global site tag (gtag.js) - Google Ads: 10827480857 -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10827480857">$
         <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag(\'js\', new Date());
-        
-          gtag(\'config\', \'AW-10823913442\');
+
+          gtag(\'config\', \'AW-10827480857\');
         </script>
       '),
   ])
