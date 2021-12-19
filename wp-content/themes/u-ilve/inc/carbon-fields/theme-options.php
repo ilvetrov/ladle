6213982a5b24294ff490b4fef9b110955230a1e4
ilvetrov <ilya.ilvetrov@gmail.com>
2021-12-19 21:39:29 +0400

    dynamic metrics

--- wp-content/themes/u-ilve/inc/carbon-fields/theme-options.php
+++ wp-content/themes/u-ilve/inc/carbon-fields/theme-options.php
@@ -38,7 +38,23 @@ function crb_attach_theme_options()
     Field::make('text', 'lead_recipients', 'Получатели заявок')
       ->set_help_text('Адрес Email, на который будут приходить заявки. Можно указать несколько через запятую.'),
     Field::make('text', 'map_code', 'Код карты Яндекс')
-      ->set_help_text('Вставьте код карты, которую нужно вывести в блоке Контактов.')
+      ->set_help_text('Вставьте код карты, которую нужно вывести в блоке Контактов.'),
+    Field::make('textarea', 'metrics_code', 'Код метрик')
+      ->set_rows(10)
+      ->set_help_text('Вставьте код всех метрик для сайта')
+      ->set_default_value('
+      <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(86836258, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/86836258" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+
+        <!-- Global site tag (gtag.js) - Google Ads: 10823913442 -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10823913442"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag(\'js\', new Date());
+        
+          gtag(\'config\', \'AW-10823913442\');
+        </script>
+      '),
   ])
   ->add_tab('Квиз', [
     Field::make('text', 'quiz_final_title', 'Заголовок финальной секции')
