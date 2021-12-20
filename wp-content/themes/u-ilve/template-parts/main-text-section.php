51eb259e008b2f2eb23adbab5a801862ed217efd
ilvetrov <ilya.ilvetrov@gmail.com>
2021-12-20 16:42:09 +0400

    bug fix of main text section in technique

--- wp-content/themes/u-ilve/template-parts/main-text-section.php
+++ wp-content/themes/u-ilve/template-parts/main-text-section.php
@@ -2,11 +2,19 @@
   <div class="container-size">
     <div class="main-text-section__container">
       <h2 class="main-text-section__title rich-content">
-        <?php echo_rich_text(carbon_get_cached_theme_option('seo_section_title')); ?>
+        <?php if (get_the_ID() && carbon_get_cached_post_meta(get_the_ID(), 'seo_section_local_text')) { ?>
+          <?php echo_rich_text(carbon_get_cached_post_meta(get_the_ID(), 'seo_section_local_title')); ?>
+        <?php } else { ?>
+          <?php echo_rich_text(carbon_get_cached_theme_option('seo_section_title')); ?>
+        <?php } ?>
       </h2>
       <!-- /.main-text-section__title -->
       <h2 class="main-text-section__text rich-content">
-        <?php echo_rich_text(carbon_get_cached_theme_option('seo_section_text')); ?>
+        <?php if (get_the_ID() && carbon_get_cached_post_meta(get_the_ID(), 'seo_section_local_text')) { ?>
+          <?php echo_rich_text(carbon_get_cached_post_meta(get_the_ID(), 'seo_section_local_text')); ?>
+        <?php } else { ?>
+          <?php echo_rich_text(carbon_get_cached_theme_option('seo_section_text')); ?>
+        <?php } ?>
       </h2>
       <!-- /.main-text-section__text -->
     </div>
